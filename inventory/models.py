acc91c5cb96edff9f00ecf9ca171e77f12528d44
Serg <sergey.panasenko@gmail.com>
2019-10-26 13:09:02 +0300

    add image model

--- inventory/models.py
+++ inventory/models.py
@@ -5,6 +5,18 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class Image(models.Model):
+    """ Image model """
+    image = ImageField(upload_to='images', verbose_name=_('image'))
+
+    def __str__(self):
+        return _('image') + '-' + str(self.id)
+
+    class Meta:
+        verbose_name = _('image')
+        verbose_name_plural = _('images')
+
+
 class Item(models.Model):
     ''' Item model - All thing in the boxes are described as an item. '''
     name = models.CharField(verbose_name=_('Name'), max_length=64)
@@ -15,6 +27,7 @@ class Item(models.Model):
     part_number = models.CharField(verbose_name=_('Part number'),
                                    max_length=32, null=True, blank=True)
     notes = models.TextField(verbose_name=_('Notes'), null=True, blank=True)
+    images = models.ManyToManyField(Image, blank=True, verbose_name=_('Image'))
 
     def __str__(self):
         return self.name
